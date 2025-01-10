898e2c0339815f69cb0c14f6893f08c4734b22ff
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-01-10 13:55:47 +0530

    completed the mario game

---
+++ day5/marioscript.js
@@ -0,0 +1,13 @@
+function moveMario() {
+  const mario = document.getElementById("mario");
+  let position = 0;
+  setInterval(() => {
+    if (screen.width == position) {
+      position = 0;
+    }
+    position += 5;
+    mario.style.marginLeft = `${position}px`;
+  }, 10);
+}
+
+moveMario();
